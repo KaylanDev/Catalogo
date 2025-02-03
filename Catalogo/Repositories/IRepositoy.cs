6f3eef12812d6a22086d58af8530cc96e7e2b1ba
KaylanDev <k1ndealblack@gmail.com>
2025-02-02 23:12:51 -0300

    feat: criando repository genetico

---
+++ Catalogo/Repositories/IRepositoy.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Catalogo.Repositories
+{
+    public interface IRepositoy<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Expression <Func<T,bool>> predicate);
+        T Create(T entity);
+        T Update(T entity);
+        T delete(T entity);
+
+    }
+}
---
+++ Catalogo/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Catalogo.Data;
+using System.Linq.Expressions;
+
+namespace Catalogo.Repositories
+{
+    public class Repository<T> : IRepositoy<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        public T Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
