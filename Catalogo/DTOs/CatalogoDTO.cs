866607f824365dbfe02e3169c6f1757b8590a333
KaylanDev <k1ndealblack@gmail.com>
2025-02-10 20:51:53 -0300

    criando DTO categoria

---
+++ Catalogo/DTOs/CatalogoDTO.cs
@@ -0,0 +1,23 @@
+﻿using Catalogo.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalogo.DTOs
+{
+    public class CatalogoDTO
+    {
+
+        
+        public int CategoriaId { get; set; }
+        
+        public string? Nome { get; set; }
+        
+        public string? ImagemUrl { get; set; }
+
+
+        public ICollection<Produto> Produtos { get; set; }
+
+    
+
+    }
+}
