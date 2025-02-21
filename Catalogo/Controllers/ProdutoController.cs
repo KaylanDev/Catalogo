bc06ca09add769ecab8af77a1e1712e485b2d217
KaylanDev <k1ndealblack@gmail.com>
2025-02-20 23:56:57 -0300

    aplicando no controller produto

--- Catalogo/Controllers/ProdutoController.cs
+++ Catalogo/Controllers/ProdutoController.cs
@@ -1,16 +1,24 @@
 ﻿using AutoMapper;
+using Azure;
 using Azure.Core;
 using Catalogo.Data;
 using Catalogo.DTOs;
 using Catalogo.Migrations;
 using Catalogo.Models;
+using Catalogo.Paginations;
 using Catalogo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Converters;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using JsonConverter = Newtonsoft.Json.JsonConverter;
+
 
 namespace Catalogo.Controllers
 {
@@ -24,10 +32,10 @@ namespace Catalogo.Controllers
         private readonly ILogger<ProdutoController> _logger;
         private readonly IMapper _mapper;
 
-        public ProdutoController(IUnitOfWork uof, ILogger<ProdutoController> logger,IMapper mapper)
+        public ProdutoController(IUnitOfWork uof, ILogger<ProdutoController> logger, IMapper mapper)
         {
             _uof = uof;
-            _logger = logger;   
+            _logger = logger;
             _mapper = mapper;
         }
 
@@ -47,7 +55,7 @@ namespace Catalogo.Controllers
             return Ok(produtosDto);
 
         }
-        [HttpGet("produtos/{id}")]
+        [HttpGet("produtosCategoria/{id}")]
         public ActionResult<IEnumerable<ProdutosDTO>> GetProdutosCategoria(int id)
         {
             var produtos = _uof.ProductRepository.GetProdutosPorCategoria(id);
@@ -60,14 +68,69 @@ namespace Catalogo.Controllers
         /// <summary>
         /// retorna elemento pelo id
         /// </summary>
-        //metodo que ira retornar pelo Id
+        /// 
         [HttpGet("{id:int}", Name = "ProdutoporID")]
         public ActionResult<ProdutosDTO> GetById(int id)
+
         {
             var produto = _uof.ProductRepository.GetById(p => p.ProdutoId == id);
             var produtoDto = _mapper.Map<ProdutosDTO>(produto);
 
             return Ok(produtoDto);
+        }
+
+        [HttpGet("paramans")]
+        public ActionResult<IEnumerable<ProdutosDTO>> GetParamns([FromQuery]ProdutosParameters produtosParameters)
+        {
+            var produtos = _uof.ProductRepository.GetPagination(produtosParameters);
+            return ObterProduto(produtos);
+        }
+        [HttpGet("Produtos/Filtros")]
+        public ActionResult<IEnumerable<ProdutosDTO>> GetFiltro([FromQuery] ProdutosFiltroPrecos produtosFiltroPrecos)
+        {
+            var produtos = _uof.ProductRepository.GetProdutosFiltro(produtosFiltroPrecos);
+            return ObterProduto(produtos);
+        }
+
+        private ActionResult<IEnumerable<ProdutosDTO>> ObterProduto(PagedList<Produtos> produtos)
+        {
+            var metaDados = new
+            {
+                produtos.TotalCount,
+                produtos.PageSize,
+                produtos.CurrentPage,
+                produtos.TotalPage,
+                produtos.HasNext,
+                produtos.HasPrevious
+            };
+
+            Response.Headers.Append("X-Append", JsonConvert.SerializeObject(metaDados));
+
+            var produtosDto = _mapper.Map<IEnumerable<ProdutosDTO>>(produtos);
+            return Ok(produtosDto);
+        }
+
+        [HttpPatch("{id:int}/UpdatePartial")]
+        public ActionResult<ProdutoDTOUpdateResponse> Patch(int id,JsonPatchDocument<ProdutoDTOUpdateRequest> patchProdutoDto)
+        {
+            if (patchProdutoDto is null || id == 0) return BadRequest();
+
+            var produto = _uof.ProductRepository.GetById(c => c.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound();
+            }
+
+            var produtoUpdateRequest = _mapper.Map<ProdutoDTOUpdateRequest>(produto);
+            patchProdutoDto.ApplyTo(produtoUpdateRequest);
+            if (!ModelState.IsValid || TryValidateModel(produtoUpdateRequest)) return BadRequest(ModelState);
+            
+
+            _mapper.Map(produtoUpdateRequest, produto);
+            _uof.ProductRepository.Update(produto);
+            _uof.Commit();
+            return Ok(_mapper.Map<ProdutoDTOUpdateResponse>(produto));
         }
 
         /// <summary>
@@ -87,7 +150,7 @@ namespace Catalogo.Controllers
             _uof.ProductRepository.Create(produto);
             _uof.Commit();
             var produtoDTo = _mapper.Map<ProdutosDTO>(produto);
-            return new CreatedAtRouteResult("obterproduto",
+            return new CreatedAtRouteResult("ProdutoporID",
                 new { Id = produtoDTo.ProdutoId }, produtoDTo);
         }
 
