cd088d35d6c8edf5778154e35ddaa957542ca960
KaylanDev <k1ndealblack@gmail.com>
2025-04-25 06:12:46 -0300

    feat: add GetTeste

---
+++ CatalogoxUnitTestes/GetProdutosUnitTeste.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogo.Controllers;
+using CatalogoxUnitTestes.xUnitTeste;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit.Sdk;
+
+namespace CatalogoxUnitTestes;
+
+public class GetProdutosxUnitTeste : IClassFixture<ProdutosTestexUnitController>
+{
+    private readonly ProdutoController _controller;
+
+    public GetProdutosxUnitTeste(ProdutosTestexUnitController controller)
+    {
+       
+ 
+            _controller = new ProdutoController(controller.repository, controller.mapper);
+        }
+
+    [Fact]
+    public async Task GetProdutoById_Return_OkResult()
+    {
+        //Arrange
+        var PId = 2;
+
+        //Act
+        var data = await _controller.GetById(PId);
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be(200);
+
+    }
+    [Fact]
+    public async Task GetProdutoById_Return_NotFound()
+    {
+        //Arrange
+        var PId = 999;
+
+        //Act
+        var data = await _controller.GetById(PId);
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<NotFoundResult>().Which.StatusCode.Should().Be(404);
+
+    }
+
+    [Fact]
+    public async Task GetProdutoById_Return_BadRequest()
+    {
+        //Arrange
+        var PId = 0;
+
+        //Act
+        var data = await _controller.GetById(PId);
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<BadRequestObjectResult>().Which.StatusCode.Should().Be(400);
+
+    }
+
+
+    //GET
+    [Fact]
+    public async Task GetProduto_Return_OkResult()
+    {
+      
+
+        //Act
+        var data = await _controller.Get();
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be(200);
+
+    }
+
+    [Fact]
+    public async Task GetProduto_Return_NotFound()
+    {
+
+
+        //Act
+        var data = await _controller.Get();
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<NotFoundResult>().Which.StatusCode.Should().Be(404);
+
+    }
+
+    [Fact]
+    public async Task GetProduto_Return_BadRequest()
+    {
+
+
+        //Act
+        var data = await _controller.Get();
+
+        ////Assert
+        //var okResult = Assert.IsType<OkObjectResult>(data.Result);
+        //Assert.Equal(200, okResult.StatusCode);
+
+        //Assert(fluent)
+        data.Result.Should().BeOfType<BadRequestResult>().Which.StatusCode.Should().Be(400);
+
+    }
+
+
+
+
+}
---
+++ CatalogoxUnitTestes/xUnitTeste/ProdutosTestexUnitController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Catalogo.Data;
+using Catalogo.DTOs.Mappins;
+using Catalogo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoxUnitTestes.xUnitTeste;
+
+public class ProdutosTestexUnitController
+{
+    public IUnitOfWork repository;
+    public IMapper mapper;
+   
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+    public static string conectionString = "Server=localhost;Database=apicatalogo;Uid=root;Pwd=Kayzin205;";
+
+    static ProdutosTestexUnitController()
+    {
+       
+        var config = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddUserSecrets<ProdutosTestexUnitController>() // pega os segredos
+           .Build();
+
+        
+
+        dbContextOptions = new DbContextOptionsBuilder<AppDbContext>().UseMySql(conectionString, ServerVersion.AutoDetect(conectionString)).Options;
+
+    }
+
+    public ProdutosTestexUnitController()
+    {
+        var configMap = new MapperConfiguration(cfg =>
+        cfg.AddProfile(new ProdutosDTOMappingProfile()));
+
+        mapper = configMap.CreateMapper();
+        var context = new AppDbContext(dbContextOptions);
+        repository = new UnitOfWork(context);
+    }
+}
